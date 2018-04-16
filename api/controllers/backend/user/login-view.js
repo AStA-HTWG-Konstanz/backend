e19cc9d199fa26908cc628eae6d75f308d73538d
Manuel Haug <kontakt@manuel-haug.de>
2018-04-16 17:36:45 +0200

    Added login and setup page

---
+++ api/controllers/backend/user/login-view.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+    friendlyName: 'User login',
+
+    description: 'Provide login page.',
+
+    inputs: {},
+
+    exits: {
+        success: {
+            responseType: 'view',
+            viewTemplatePath: 'pages/login'
+        },
+        noUserExists: {
+            description: 'There is no user.',
+            responseType: 'redirect'
+        }
+    },
+
+    fn: async function (inputs, exits) {
+        let backendUser = await BackendUser.countUser();
+        if (backendUser === 0) {
+            return exits.noUserExists("/setup");
+        }
+        return exits.success();
+    }
+};
---
+++ api/controllers/backend/user/login.js
@@ -0,0 +1,59 @@
+module.exports = {
+
+    friendlyName: 'Login user',
+
+    description: 'Provide page to login.',
+
+    inputs: {
+        username: {
+            type: 'string',
+            example: 'user1',
+            description: 'Username for datacenter login at HTWG Konstanz.'
+        },
+        password: {
+            type: 'string',
+            example: 'password1234',
+            description: 'Password for datacenter login at HTWG Konstanz.'
+        }
+    },
+
+    exits: {
+        success: {
+            responseType: 'redirect'
+        },
+        emptyParameters: {
+            responseType: 'view',
+            viewTemplatePath: 'pages/login'
+        },
+        errorOccured: {
+            responseType: 'view',
+            viewTemplatePath: 'pages/login'
+        }
+    },
+
+    fn: async function (inputs, exits) {
+        if (inputs.username === "" || inputs.password === "") {
+            return exits.emptyParameters({errorMessage: "Username/Password can't be empty."});
+        }
+        let username = inputs.username;
+        let password = inputs.password;
+
+        sails.helpers.ldapLogin(username, password).then((user) => {
+            BackendUser.findUser({username: user["uid"]}).then((backendUser) => {
+                this.req.session.username = user["uid"];
+                this.req.session.name = user["cn"];
+                if (backendUser.admin) {
+                    this.req.session.role = "admin";
+                } else {
+                    this.req.session.role = "editor";
+                }
+                return exits.success("/");
+            }).catch(function (error) {
+                sails.log(error);
+                return exits.errorOccured({errorMessage: "You don't have enough rights to login."});
+            });
+        }).catch(function (error) {
+            return exits.errorOccured({errorMessage: "Login failed. Please check your Username/Password or try again later."});
+        });
+    }
+};
