a0756efa053622c1407cbd836a1286c3eef9950e
gitBear11 <romanleinweber@googlemail.com>
2018-04-18 16:57:05 +0200

    created ldap controller

---
+++ api/controllers/api/user/auth.js
@@ -0,0 +1,50 @@
+module.exports = {
+
+
+  friendlyName: 'LDAP Auth',
+
+  description: 'LDAP-User authentication',
+
+  inputs: {
+    username: {
+      type: 'string',
+      required: true,
+      example: 'user1',
+      description: 'Username for datacenter login at HTWG Konstanz.'
+    },
+    password: {
+      type: 'string',
+      required: true,
+      example: 'password1234',
+      description: 'Password for datacenter login at HTWG Konstanz.'
+    }
+  },
+
+  exits: {
+      success: {
+        statusCode: 200,
+        responseType: ""
+      },
+      failure: {
+      statusCode: 400,
+      responseType: ""
+    }
+  },
+
+  fn: async function (inputs, exits) {
+    let username = inputs.username;
+    let password = inputs.password;
+
+      sails.helpers.ldapLogin(username, password).then((user) => {
+        this.req.session.logged = true;
+        return exits.success();
+        }).catch(function (error){
+         return exits.failure();
+      });
+
+
+
+
+  }
+
+};
