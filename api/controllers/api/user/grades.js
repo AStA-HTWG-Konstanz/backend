ccf2dbb4c2f8e665b37ca75186bb1f74a80ab6eb
Manuel Haug <kontakt@manuel-haug.de>
2018-05-17 18:15:46 +0200

    Added grade output route for app

--- api/controllers/api/user/grades.js
+++ api/controllers/api/user/grades.js
@@ -19,12 +19,36 @@ module.exports = {
         },
         errorOccured: {
             statusCode: 500,
+        },
+        noData: {
+            statusCode: 204
         }
     },
 
     fn: async function (inputs, exits) {
         let token = inputs.token;
-
+        let output = {gradesReport: {}};
+        let gradeData = await QisGrades.find({token: token}).populate("semester").populate("course").sort("id ASC").catch((e) => {
+            sails.log.error(e);
+            return exits.errorOccured();
+        });
+        if (gradeData) {
+            for (let i in gradeData) {
+                if (typeof output.gradesReport[gradeData[i].semester.semester] === "undefined") {
+                    output.gradesReport[gradeData[i].semester.semester] = [];
+                }
+                output.gradesReport[gradeData[i].semester.semester].push({
+                    course: gradeData[i].course.course,
+                    name: gradeData[i].name,
+                    grade: gradeData[i].grade.toString().replace(".", ","),
+                    ects: gradeData[i].ects.toString().replace(".", ","),
+                    passed: gradeData[i].passed,
+                });
+            }
+            return exits.success(output);
+        } else {
+            return exits.noData({gradesReport: []});
+        }
 
     }
 };
