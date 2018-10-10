50301a9d2ba51885f1747e49f7acc9fb10062213
Manuel Haug <kontakt@manuel-haug.de>
2018-10-10 03:30:00 +0200

    revert back to cookie base solution

--- api/policies/isAllowedToRefresh.js
+++ api/policies/isAllowedToRefresh.js
@@ -1,43 +1,18 @@
 const oneWeek = 60 * 60 * 168 * 1000;
-const moment = require('moment');
-
 module.exports = async function (req, res, proceed) {
 
-    //check if required parameter is present
-    if (typeof req.body.token !== "undefined") {
-        let token = req.body.token;
-        //get latest access from database
-        let latestAccess = await QisGradesRefresh.findOne({token: req.body.token}).catch(function (e) {
-            sails.log.error(e);
-            return res.serverError();
-        });
-
-        let dateToday = moment().format('YYYY-MM-DD');
-
-        if (latestAccess) {
-            //if latestAccess is found check if older than 7 days
-            let now = new Date();
-            if ((now - new Date(latestAccess.lastRefreshDate)) > oneWeek) {
-                //set today as new last accessed date
-                QisGradesRefresh.update({token: token}).set({lastRefreshDate: dateToday}).then(() => {
-                    return proceed();
-                }).catch(function (e) {
-                    sails.log.error(e);
-                    res.serverError();
-                });
-            } else {
-                return res.ok();
-            }
-        } else {
-            //if no latest access is found a new one is created
-            QisGradesRefresh.create({token: token, lastRefreshDate: dateToday}).then(() => {
-                return proceed();
-            }).catch(function (e) {
-                sails.log.error(e);
-                return res.serverError();
-            });
+    if (req.session.APIlastRefresh) {
+        let lastAccess = new Date(req.session.APIlastRefresh);
+        let now = new Date();
+        //if older than 7 days
+        if ((now - lastAccess) > oneWeek) {
+            req.session.APIlastRefresh = new Date().toUTCString();
+            return proceed();
         }
-    } else {
-        return res.serverError();
+        //else
+        return res.ok();
     }
+    //set to today
+    req.session.APIlastRefresh = new Date().toUTCString();
+    return proceed();
 };
