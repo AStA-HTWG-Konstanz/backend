7be569e447ba937c2bd2a6fdb6ec6fc2b335ea1b
Manuel Haug <kontakt@manuel-haug.de>
2018-10-10 12:15:14 +0200

    Change date conversion and minor changes

--- api/policies/isAllowedToRefresh.js
+++ api/policies/isAllowedToRefresh.js
@@ -1,5 +1,6 @@
 const oneWeek = 60 * 60 * 168 * 1000;
-const options = {year: 'numeric', month: '2-digit', day: '2-digit'};
+const moment = require('moment');
+
 module.exports = async function (req, res, proceed) {
 
     //check if required parameter is present
@@ -11,27 +12,30 @@ module.exports = async function (req, res, proceed) {
             return res.serverError();
         });
 
-        let dateToday = new Date().toJSON().slice(0, 10);
+        let dateToday = moment().format('YYYY-MM-DD');
 
         if (latestAccess) {
             //if latestAccess is found check if older than 7 days
             let now = new Date();
             if ((now - new Date(latestAccess.lastRefreshDate)) > oneWeek) {
                 //set today as new last accessed date
-                await QisGradesRefresh.update({token: token},{lastRefreshDate: dateToday}).catch(function (e) {
+                QisGradesRefresh.update({token: token}).set({lastRefreshDate: dateToday}).then(() => {
+                    return proceed();
+                }).catch(function (e) {
                     sails.log.error(e);
                     res.serverError();
                 });
-                return proceed();
+            } else {
+                return res.ok();
             }
-            return res.ok();
         } else {
             //if no latest access is found a new one is created
-            await QisGradesRefresh.create({token: token, lastRefreshDate: dateToday}).catch(function (e) {
+            QisGradesRefresh.create({token: token, lastRefreshDate: dateToday}).then(() => {
+                return proceed();
+            }).catch(function (e) {
                 sails.log.error(e);
                 return res.serverError();
             });
-            return proceed();
         }
     } else {
         return res.serverError();
