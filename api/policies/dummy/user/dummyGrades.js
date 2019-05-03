f61de1a4111b41cf7260022a34235580bc9fc686
da721wol <davwol@gmx.de>
2019-05-03 11:35:20 +0200

    returned correct json

--- api/policies/dummy/user/dummyGrades.js
+++ api/policies/dummy/user/dummyGrades.js
@@ -2,7 +2,7 @@ const grades = require('../../../../json/grades');
 module.exports = async function (req, res, proceed) {
 
     if (req.session.APIusername === "testUser") {
-        return res.ok(balance);
+        return res.ok(grades);
     } else {
         return proceed();
     }
