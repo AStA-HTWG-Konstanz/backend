184e158b580f5ae630884d4f47bc5fc50b8d19d1
Matthias Asche <matthiaskg.asche@gmail.com>
2019-05-06 11:04:01 +0200

    changed request method, included cheerio, added error

--- api/controllers/api/strandbar/strandbar.js
+++ api/controllers/api/strandbar/strandbar.js
@@ -1,48 +1,53 @@
-const jsdom = require("jsdom");
-const {JSDOM} = jsdom;
-var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-global.DOMParser = new JSDOM().window.DOMParser;
+let request = require('request');
+const cheerio = require('cheerio');
 
 module.exports = {
-  friendlyName: 'strandbar',
+    friendlyName: 'strandbar',
 
-  description: 'checks if the strandbar is open',
+    description: 'checks if the strandbar is open',
 
-  inputs: {},
+    inputs: {},
 
-  exits: {
-    success: {
-      statusCode: 200
-    },
-    invalidRequest: {
-      statusCode: 400
-    },
-    errorOccured: {
-      statusCode: 500,
-      responseType: ''
-    }
-  },
-  fn: async function (inputs, exits) {
-    const HTTP = new XMLHttpRequest();
-    HTTP.open("GET", sails.config.custom.strandbar.urlopen);
-    HTTP.send();
-    try {
-      HTTP.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-          const response = JSON.parse(HTTP.responseText);
-          if (response.collection.description.includes("Geöffnet")) {
-            return exits.success({open: true});
-          } else if (response.collection.description.includes("Geschlossen")) {
-            return exits.success({open: false});
-          }
+    exits: {
+        success: {
+            statusCode: 200
+        },
+        invalidRequest: {
+            statusCode: 400
+        },
+        errorOccured: {
+            statusCode: 500,
+            responseType: ''
         }
-      }
-    } catch (error) {
-      sails.log.error(error);
-      return exits.failure();
+    },
+    fn: async function (inputs, exits) {
+
+        request.get({
+            url: sails.config.custom.strandbar.urlopen,
+            headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0'}
+        }, function (err, httpResponse, body) {
+            if (err) {
+                sails.log.error(err);
+                return exits.errorOccured();
+            }
+            try {
+                const $ = cheerio.load(body);
+                let open = $("strong:nth-child(1)").text();
+                if (open === "Geschlossen") {
+                    return exits.success({open: false});
+                } else if (open === "Geöffnet") {
+                    return exits.success({open: true});
+                } else {
+                    sails.log.error("Strandbar - elemnt not found");
+                    return exits.failure();
+                }
+            } catch (error) {
+                sails.log.error(error);
+                return exits.failure();
+            }
+
+        });
 
     }
-
-  }
 
 };
