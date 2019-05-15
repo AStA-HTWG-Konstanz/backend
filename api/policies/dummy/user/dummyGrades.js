e256f2924fcfb7c1d081b68e1bb627ecae885794
da721wol <davwol@gmx.de>
2019-05-15 12:36:43 +0200

    fix

--- api/policies/dummy/user/dummyGrades.js
+++ api/policies/dummy/user/dummyGrades.js
@@ -1,8 +1,14 @@
 module.exports = async function (req, res, proceed) {
+<<<<<<< Updated upstream
     if (req.session.APIusername === "testUser") {
         res.status(204);
         return res.end();
+=======
+    return res.ok(grades);
+
+    /*if (req.session.APIusername === "testUser") {
+>>>>>>> Stashed changes
     } else {
         return proceed();
-    }
+    }*/
 };
