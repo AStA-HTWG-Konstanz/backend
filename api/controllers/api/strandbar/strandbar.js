84deb88bb514603b3d8d3371839dc86723814133
Matthias Asche <matthiaskg.asche@gmail.com>
2019-05-23 11:11:42 +0200

    removed unused stuff

--- api/controllers/api/strandbar/strandbar.js
+++ api/controllers/api/strandbar/strandbar.js
@@ -37,52 +37,6 @@ module.exports = {
             sails.log.error(error);
             return exits.failure;
         }
-
-
-        /*
-
-        try {
-            let value = await sails.getDatastore('redisCache').leaseConnection(async (db) => {
-                let found = await (util.promisify(db.get).bind(db))('strandbar');
-                if (found === null) {
-                    return exits.failure;
-                } else {
-                    return exits.success({open: value})
-                }
-            })
-        } catch (error) {
-            sails.log.error(error);
-            return exits.failure;
-        }
-
-        /*
-
-        request.get({
-            url: sails.config.custom.strandbar.urlopen,
-            headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0'}
-        }, function (err, httpResponse, body) {
-            if (err) {
-                sails.log.error(err);
-                return exits.errorOccured();
-            }
-            try {
-                const $ = cheerio.load(body);
-                let open = $("strong:nth-child(1)").text();
-                if (open === "Geschlossen") {
-                    return exits.success({open: false});
-                } else if (open === "Geöffnet") {
-                    return exits.success({open: true});
-                } else {
-                    sails.log.error("Strandbar - element not found");
-                    return exits.failure();
-                }
-            } catch (error) {
-                sails.log.error(error);
-                return exits.failure();
-            }
-
-        });*/
-
     }
 
 };
