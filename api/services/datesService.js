c5767d814e71eeed60ec90933ed2387b4c9f726f
Matthias Asche <matthiaskg.asche@gmail.com>
2019-07-05 19:02:52 +0200

    added logging in dates job

--- api/services/datesService.js
+++ api/services/datesService.js
@@ -27,6 +27,7 @@ module.exports = {
     },
 
     datesJob: async function (inputs, exits) {
+        sails.log.info("starting dates job");
         request.get({
             url: sails.config.custom.events.urlopen,
             headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0'}
