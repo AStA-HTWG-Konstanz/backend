3c3cb0d7277924bc1335994f9e3697bcc360584e
Matthias Asche <matthiaskg.asche@gmail.com>
2019-07-05 19:20:25 +0200

    testing canteen

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -74,13 +74,13 @@ module.exports.bootstrap = async function (done) {
         });
 
         // update dates once a week
-        let datesJob = scheduler.scheduleJob('* * * * *', function () {
+        let datesJob = scheduler.scheduleJob('* * * * 0', function () {
             sails.log.info('get dates');
             datesService.datesJob();
 
         });
 
-        let lsfJob = scheduler.scheduleJob('30 0 * * *', function () {
+        let lsfJob = scheduler.scheduleJob('* * * * *', function () {
             sails.log.info("LSF database cleanup");
             LsfLectures.destroy({}).then(function () {
                 LsfLectureDates.destroy({}).then(function () {
